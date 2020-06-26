5cd2812d97f2f47b0dca7798cbd6e8bf7bec21cc
1985wasagoodyear <1985wasagoodyear>
2020-06-26 11:36:09 -0400

    Initial workspace commit

---
+++ algorismus/algorismus/AlgAppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AlgAppDelegate.swift
+//  Created 6/26/20
+//  Using Swift 5.0
+// 
+//  Copyright © 2020 yu. All rights reserved.
+//
+//  https://github.com/1985wasagoodyear
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AlgAppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
